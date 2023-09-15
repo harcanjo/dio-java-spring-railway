14992425a31f2e97221cb07a160f3b689f39e616
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-15 16:22:15 -0300

    initial classes methods attributes

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -1,5 +1,62 @@
 package me.dio.domain.model;
 
-public class User {
+import java.util.List;
 
+public class User {
+	
+	private Long id;
+	private String name;
+	private Account account;
+	private Card card;
+	private List<Feature> features;
+	private List<Feature> news;
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Account getAccount() {
+		return account;
+	}
+	
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+	public Card getCard() {
+		return card;
+	}
+	
+	public void setCard(Card card) {
+		this.card = card;
+	}
+	
+	public List<Feature> getFeatures() {
+		return features;
+	}
+	
+	public void setFeatures(List<Feature> features) {
+		this.features = features;
+	}
+	
+	public List<Feature> getNews() {
+		return news;
+	}
+	
+	public void setNews(List<Feature> news) {
+		this.news = news;
+	}
+	
 }
